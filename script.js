99a2f99153629cb0fa33bb646c18174bda9f5bd4
pamp the horn <pampthehorn@gmail.com>
2020-07-05 13:16:02 +0100

    is that logo width minus 50 thing still needed to stop overflowX on iphone

--- script.js
+++ script.js
@@ -24,15 +24,15 @@ function effect(elClass){
             clearInterval(interval);
             init()
       }
-  },50000/el.innerHTML.length)
+  },10000/el.innerHTML.length)
   }
   
   function zoomFit(elClass){
           var el = document.getElementsByClassName(elClass)[0];
           var elParent = el.parentNode;
           el.style.fontSize = '20px';
-          while((window.innerWidth-50<el.getBoundingClientRect().width || 
-          el.getBoundingClientRect().height-50 > elParent.offsetHeight) && 
+          while((window.innerWidth<el.getBoundingClientRect().width || 
+          el.getBoundingClientRect().height + elParent.offsetTop > elParent.offsetHeight) && 
           parseFloat(el.style.fontSize.split('px')[0])>5)
           {
                   el.style.fontSize = (parseFloat(el.style.fontSize.split('px')[0])-1).toString()+'px';
@@ -50,6 +50,8 @@ function effect(elClass){
   
   var randomColours = ['chocolate','cornflowerblue','thistle','tomato','pink','yellowgreen']
   document.body.style.backgroundColor = randomColours[Math.floor(randomColours.length*Math.random())];
+  //document.getElementById('').style.backgroundColor = randomColours[Math.floor(randomColours.length*Math.random())];
+
   
   function init(){
     zoomFit('zoomFit');
@@ -61,6 +63,8 @@ window.onload = function(){
 window.onresize = function(){
     zoomFit('zoomFit');
 }
+
 zoomFit('zoomFit');
 effect('effect');
 zoomFit('zoomFit');
+
