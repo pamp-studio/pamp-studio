d035a8799b753c0d9ec2bcef04e2a5e2ae1d85e6
pamp the horn <pampthehorn@gmail.com>
2020-09-12 12:23:31 +0100

    maybe index.html doesn't need to be in the manigest

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
-var cacheHistory = ['v0','v0.01'];
-var cacheName = 'v0.01'
+var cacheHistory = ['v0','v0.01','v0.02'];
+var cacheName = 'v0.02'
 var urlsToCache = [
-  'index.html',
   'js/figlet.js',
   'js/ASCIILogoGenerator.js',
   'js/script.js',
