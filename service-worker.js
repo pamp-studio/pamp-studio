7f91190a81225235f12d162490147eac367fa5cc
pamp the horn <pampthehorn@gmail.com>
2020-10-16 13:12:34 +0100

    cache ffs

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
-var cacheHistory = ['v0','v0.01','v0.02','v0.03','v0.04','v0.05','v0.06','v0.07','v0.08','v0.09'];
-var cacheName = 'v0.10';
+var cacheHistory = ['v0','v0.01','v0.02','v0.03','v0.04','v0.05','v0.06','v0.07','v0.08','v0.09','v0.10'];
+var cacheName = 'v0.11';
 var urlsToCache = [
+  '/',
   'index.html',
   'style.css',
   'js/figlet.js',
